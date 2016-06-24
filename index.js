c5df8e23d40a1e6bd4b9924badb09d4bb7dce0b4
Gaurav Gupta <gaurav.gupta5@tothenew.com>
2016-06-25 02:34:59 +0530

    Handled error when cloudinary was returning back error. Deleted the file.

--- index.js
+++ index.js
@@ -11,18 +11,36 @@ function CloudinaryStorage (opts) {
   }else{
     throw new Error('Expected opts.cloudinary to be a configured cloudinary object.');
   }
-  switch (typeof opts.s3StreamParams) {
-    case 'function': this.getParams = opts.s3StreamParams; break;
-    case 'undefined': this.getParams = function () {return;};break;
+  var paramTemplate = {
+    remove: null,
+    upload: null
+  }
+  switch (typeof opts.params) {
+    case 'function': this.getParams = opts.params; break;
+    case 'undefined': this.getParams = function () {return paramTemplate;};break;
     default: throw new TypeError('Expected opts.key to be undefined or function')
   }
 }
 
 CloudinaryStorage.prototype._handleFile = function _handleFile (req, file, cb) {
-  var self = this;
-  var cloudinaryStream = self.cloudinary.uploader.upload_stream(function(result) { cb(null, result)}, self.getParams());
+  var cloudinaryStream = this.cloudinary.uploader.upload_stream(function(result) {
+      if(!result || result.error){
+        return cb(result.error.message);
+      }
+      cb(null, result)
+    } , this.getParams().upload);
   file.stream.pipe(cloudinaryStream);
 };
+
+CloudinaryStorage.prototype._removeFile = function _removeFile (req, file, cb) {
+
+  this.cloudinary.v2.uploader.destroy(file.public_id, this.getParams().remove, function (err, result) {
+    if(err){
+      return cb(err);
+    }
+    return cb(result);
+  });
+}
 
 module.exports = function (opts) {
   opts = opts || {};
